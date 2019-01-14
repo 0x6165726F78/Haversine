bf6b64fd0b0a9b7e75b0513fc69f7d9c8bb61689
aerox <aerox.javascript@hotmail.com>
2019-01-14 23:34:27 +0100

    fix: avoid multiple decimal values computations in calculateAverageValue
    Avoid computations such the direct sum of multiple decimal values.
    
    The binary format used by the computers to represent numbers, cannot represent all decimal values. A great part of them are rounded to the nearest number that can be represented.
    
    If you keep operating on values that cannot be exactly represented you'll end up adding more and more margin of error for the final computation.
    
    In this specific scenario I wanted to calculate the average value from a serie of values provided. To avoid the problem described above you can ensure that you're operating on integer values ( can be exactly represented in binary format ) by multiplying each value provided by a multiple of 10.
    
    Before returning the result that can be a decimal number you have to divide by the multiple of 10 you previously choosed.

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -4,18 +4,19 @@ const paramsSchema = joi.array().items(
   joi.number().required()
 ).required().options({ abortEarly: false, convert: false });
 
-function calculateAverageValue (values) {
+function calculateAverageValue (values, maxNumDecimals = 2) {
   joi.attempt(values, paramsSchema);
 
   const numValues = values.length;
+  const padding = 10 ** maxNumDecimals;
 
   return values.reduce((result, nextNumber, index) => {
     const lastOperation = index === numValues - 1;
 
-    result += nextNumber;
+    result += Math.round(nextNumber.toFixed(maxNumDecimals) * padding);
 
     return lastOperation
-      ? result / numValues
+      ? ((result / padding) / numValues)
       : result;
   }, 0);
 }
